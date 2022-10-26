e2b9d0cb481200bb2cf5d7a2c6288270f45c59d0
cyboranf <cyboranf@gmail.com>
2022-10-26 17:39:45 +0200

    Landing page Controller -updated

--- src/main/java/pl/project/charity/web/HomeController.java
+++ src/main/java/pl/project/charity/web/HomeController.java
@@ -3,6 +3,7 @@ package pl.project.charity.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -10,7 +11,7 @@ public class HomeController {
 
 
     @RequestMapping("/")
-    public String homeAction(Model model){
-        return "index";
+    public ModelAndView homeAction(Model model){
+        return new ModelAndView("index");
     }
 }
