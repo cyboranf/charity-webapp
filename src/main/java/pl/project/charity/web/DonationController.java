eb79d9202b308c83b9eb6aed7e00c9ad0fd88b89
cyboranf <cyboranf@gmail.com>
2022-10-28 17:17:25 +0200

    header.jsp -changed

--- src/main/java/pl/project/charity/web/DonationController.java
+++ src/main/java/pl/project/charity/web/DonationController.java
@@ -2,17 +2,21 @@ package pl.project.charity.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.project.charity.domain.Category;
 import pl.project.charity.domain.Donation;
+import pl.project.charity.domain.Institution;
 import pl.project.charity.service.CategoryService;
 import pl.project.charity.service.DonationService;
 import pl.project.charity.service.InstitutionService;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @Controller
-@RequestMapping("/donation/add")
+@RequestMapping("/donation")
+@SessionAttributes("donation")
 public class DonationController {
     private final DonationService donationService;
     private final CategoryService categoryService;
@@ -24,10 +28,38 @@ public class DonationController {
         this.institutionService = institutionService;
     }
 
-    @GetMapping("")
-    public String showForm(Model model) {
-        model.addAttribute("categories",categoryService.findAll());
+
+    @GetMapping("/add")
+    public String showForm() {
         return "form1";
+    }
+
+    @PostMapping("/add2")
+    public String postForm2(@ModelAttribute Donation donations) {
+        return "form2";
+    }
+
+    @PostMapping("/add3")
+    public String postForm3(@ModelAttribute Donation donations){
+        return "form3";
+    }
+
+
+
+
+    @ModelAttribute("categories")
+    public List<Category> categories() {
+        return categoryService.findAll();
+    }
+
+    @ModelAttribute("institutions")
+    public List<Institution> institutions() {
+        return institutionService.findAll();
+    }
+
+    @ModelAttribute("donation")
+    public Donation donationSession() {
+        return new Donation();
     }
 
 
