30480eb582619fe2a6ea5bdb1622e5e81b780372
cyboranf <cyboranf@gmail.com>
2022-11-07 15:44:21 +0100

    Donation -repaired

--- src/main/java/pl/project/charity/domain/Donation.java
+++ src/main/java/pl/project/charity/domain/Donation.java
@@ -1,8 +1,11 @@
 package pl.project.charity.domain;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -19,8 +22,10 @@ public class Donation {
     @Column(name = "quantity")
     private int quantity;
     @ManyToMany(fetch = FetchType.EAGER)
+    @NotEmpty(message = "{categories.notEmpty}")
     private List<Category> categories = new ArrayList<>();
     @ManyToOne (fetch = FetchType.EAGER)
+    @NotNull(message = "{institution.notNull}")
     private Institution institution;
     @Column(name = "street")
     private String street;
@@ -29,6 +34,7 @@ public class Donation {
     @Column(name = "zip_code")
     private  String zipCode;
     @Column(name = "date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate pickUpDate;
     @Column(name = "time")
     private LocalTime pickUpTime;
