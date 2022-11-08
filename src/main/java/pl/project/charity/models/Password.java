97def1fdb73edc0d7943023fe637d11421ebf34c
cyboranf <cyboranf@gmail.com>
2022-11-08 21:57:04 +0100

    Password model -added

---
+++ src/main/java/pl/project/charity/models/Password.java
@@ -0,0 +1,21 @@
+package pl.project.charity.models;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class Password {
+    private String email;
+
+    private String password;
+
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,100}$", message = "{weak.password}")
+    @NotEmpty(message = "{newPassword.notEmpty}")
+    private String newPassword;
+
+    private String matchingNewPassword;
+
+    private String token;
+}
