99863ab7f687d5bb48b7539a6e6d7a4b539be8a3
cyboranf <cyboranf@gmail.com>
2022-11-09 18:09:51 +0100

    Changes

--- src/main/java/pl/project/charity/web/LoginController.java
+++ src/main/java/pl/project/charity/web/LoginController.java
@@ -18,65 +18,65 @@ import javax.validation.Valid;
 @Controller
 public class LoginController {
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
+//    private final PasswordEncoder passwordEncoder;
 
-    public LoginController(UserService userService, PasswordEncoder passwordEncoder) {
+    public LoginController(UserService userService) {
         this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
+//        this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/login")
-    public String showLoginForm(Model model) {
-        model.addAttribute("user", new User());
-
-
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String LogIn(Model model,
-                        @RequestParam String email,
-                        @RequestParam String password,
-                        @ModelAttribute("user") User user,
-                        HttpServletResponse response) {
-        user=userService.findByEmail(email);
-        if (password.equals(passwordEncoder.encode(user.getPassword()))){
-            response.addCookie(new Cookie("Username",user.getFirstName()));
-            return "form";
-        }else {
-            return "";
-        }
-    }
-
-    @GetMapping("/register")
-    public String showRegisterForm(Model model) {
-        model.addAttribute("user", new User());
-        return "register";
-    }
-
-    @PostMapping("/register")
-    public String addUser(@Valid @ModelAttribute("user") User user,
-                          BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            model.addAttribute("user", user);
-            return "register";
-        }
-        if (userService.findByEmail(user.getEmail()) != null) {
-            result.rejectValue("email", "error.user", "Taki Email już istnieje");
-            model.addAttribute("user", user);
-            return "register";
-        }
-        if (!user.getPassword().equals(user.getMatchingPassword())) {
-            result.rejectValue("password", "error.user", "Podane hasłą są różne");
-            model.addAttribute("user", user);
-            return "register";
-        }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setEnabled(false);
-        user.setAccess(true);
-        userService.save(user);
-        return "login";
-    }
+//    @GetMapping("/login")
+//    public String showLoginForm(Model model) {
+//        model.addAttribute("user", new User());
+//
+//
+//        return "login";
+//    }
+//
+//    @PostMapping("/login")
+//    public String LogIn(Model model,
+//                        @RequestParam String email,
+//                        @RequestParam String password,
+//                        @ModelAttribute("user") User user,
+//                        HttpServletResponse response) {
+//        user=userService.findByEmail(email);
+//        if (password.equals(passwordEncoder.encode(user.getPassword()))){
+//            response.addCookie(new Cookie("Username",user.getFirstName()));
+//            return "form";
+//        }else {
+//            return "";
+//        }
+//    }
+//
+//    @GetMapping("/register")
+//    public String showRegisterForm(Model model) {
+//        model.addAttribute("user", new User());
+//        return "register";
+//    }
+//
+//    @PostMapping("/register")
+//    public String addUser(@Valid @ModelAttribute("user") User user,
+//                          BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            model.addAttribute("user", user);
+//            return "register";
+//        }
+//        if (userService.findByEmail(user.getEmail()) != null) {
+//            result.rejectValue("email", "error.user", "Taki Email już istnieje");
+//            model.addAttribute("user", user);
+//            return "register";
+//        }
+//        if (!user.getPassword().equals(user.getMatchingPassword())) {
+//            result.rejectValue("password", "error.user", "Podane hasłą są różne");
+//            model.addAttribute("user", user);
+//            return "register";
+//        }
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        user.setEnabled(false);
+//        user.setAccess(true);
+//        userService.save(user);
+//        return "login";
+//    }
 
 
 }
