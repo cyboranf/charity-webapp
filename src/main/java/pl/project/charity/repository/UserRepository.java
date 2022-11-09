b6579e59d16650d1cf1ff0947b50aa0e4f861137
cyboranf <cyboranf@gmail.com>
2022-11-09 17:59:37 +0100

    Repository -repaired

--- src/main/java/pl/project/charity/repository/UserRepository.java
+++ src/main/java/pl/project/charity/repository/UserRepository.java
@@ -3,16 +3,10 @@ package pl.project.charity.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.project.charity.domain.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findFirstByEmail(String email);
+
     User findFirstById(Long userId);
 
     User findByFirstName(String name);
-    void createVerificationToken(User user, String token);
-
-    void confirmUser(String token);
-
-    void createChangeEmailToken(User user, String token);
-
-    void confirmChangeEmail(String token);
 }
