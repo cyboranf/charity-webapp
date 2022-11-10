a629b94095cf5294cef72475c23eb90e1fd62883
cyboranf <cyboranf@gmail.com>
2022-11-10 23:29:32 +0100

    controllers -upgrade

--- src/main/java/pl/project/charity/web/DonationController.java
+++ src/main/java/pl/project/charity/web/DonationController.java
@@ -1,5 +1,7 @@
 package pl.project.charity.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,12 +9,13 @@ import org.springframework.web.bind.annotation.*;
 import pl.project.charity.domain.Category;
 import pl.project.charity.domain.Donation;
 import pl.project.charity.domain.Institution;
+import pl.project.charity.domain.User;
 import pl.project.charity.service.CategoryService;
 import pl.project.charity.service.DonationService;
 import pl.project.charity.service.InstitutionService;
+import pl.project.charity.service.UserService;
 
 import javax.validation.Valid;
-import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -23,26 +26,40 @@ public class DonationController {
     private final CategoryService categoryService;
     private final InstitutionService institutionService;
 
-    public DonationController(DonationService donationService, CategoryService categoryService, InstitutionService institutionService) {
+    private final UserService userService;
+
+    public DonationController(DonationService donationService, CategoryService categoryService, InstitutionService institutionService, UserService userService) {
         this.donationService = donationService;
         this.categoryService = categoryService;
         this.institutionService = institutionService;
+        this.userService = userService;
     }
 
 
     @GetMapping("/add")
-    public String donationForm(Model model) {
+    public String donationForm(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        String email=userDetails.getUsername();
+        User user=userService.findByEmail(email);
+        if (!user.isAccess()){
+            return "noAccess";
+        }
         model.addAttribute("donation", new Donation());
         return "form";
     }
 
     @PostMapping("/add")
-    public String procesDonationForm(@Valid Donation donation, BindingResult result, Model model) {
+    public String procesDonationForm(@Valid Donation donation, BindingResult result,
+                                     Model model, @AuthenticationPrincipal UserDetails userDetails) {
         if (result.hasErrors()) {
             model.addAttribute("donation", donation);
             return "form";
         }
+        String email=userDetails.getUsername();
+        User user=userService.findByEmail(email);
+        donation.setUser(user);
+        donation.setReceived(false);
         donationService.save(donation);
+
         return "formConfirmation";
     }
 
