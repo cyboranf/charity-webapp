da28cfc5775834966bfd1c932170a387c18ecea2
cyboranf <cyboranf@gmail.com>
2022-11-11 14:53:19 +0100

    changePassword and UserShow methods -added

--- src/main/java/pl/project/charity/web/UserController.java
+++ src/main/java/pl/project/charity/web/UserController.java
@@ -6,16 +6,21 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.project.charity.domain.Institution;
 import pl.project.charity.domain.User;
+import pl.project.charity.models.Password;
 import pl.project.charity.repository.ChangeTokenRepository;
 import pl.project.charity.service.AuthorityService;
 import pl.project.charity.service.DonationService;
 import pl.project.charity.service.UserService;
 import pl.project.charity.service.VerificationTokenService;
 
+import javax.validation.Valid;
 import java.util.Collection;
 import java.util.List;
 
@@ -49,6 +54,47 @@ public class UserController {
             return "admin";
         }
         return "user";
+    }
+
+    @RequestMapping("/user/profil")
+    public String userProfil(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        String email = userDetails.getUsername();
+        User user = userService.findByEmail(email);
+        model.addAttribute("user", user);
+        return "profil";
+    }
+
+    @RequestMapping("/user/changePassword/{id}")
+    public String changePasswordForm(@PathVariable long id, Model model){
+        User user=userService.findById(id);
+        Password password=new Password();
+        password.setEmail(user.getEmail());
+        model.addAttribute("password",password);
+        return "changePassword";
+    }
+
+    @GetMapping("/user/changePassword")
+    public String changePassword(@Valid Password password, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "changePassword";
+        }
+
+        User user = userService.findByEmail(password.getEmail());
+
+        if (!passwordEncoder.matches(password.getPassword(), user.getPassword())) {
+            result.rejectValue("email", "error.password", "Błąd potwierdzenia nowego hasła");
+            model.addAttribute("password", password);
+            return "changePassword";
+        }
+        if (!password.getNewPassword().equals(password.getMatchingNewPassword())){
+            result.rejectValue("email","error.password","Błąd potwierdzenia nowego hasła");
+            model.addAttribute("password",password);
+            return "changePassword";
+        }
+        user.setPassword(passwordEncoder.encode(password.getNewPassword()));
+        userService.save(user);
+        return "changePassword-confirmation";
+
     }
 
     @ModelAttribute("users")
