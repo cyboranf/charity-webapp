427e33e224baf6079ccfef9244baa603c5ed3d58
cyboranf <cyboranf@gmail.com>
2022-11-29 22:21:35 +0100

    PasswordController to email sending -prepared

--- src/main/java/pl/project/charity/web/PasswordController.java
+++ src/main/java/pl/project/charity/web/PasswordController.java
@@ -9,11 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.project.charity.models.Password;
-import pl.project.charity.service.PasswordService;
+import pl.project.charity.service.PasswordServiceImpl;
 import pl.project.charity.service.ResetTokenService;
 import pl.project.charity.service.UserService;
 
-import javax.naming.Binding;
 import javax.validation.Valid;
 
 @Data
@@ -21,7 +20,7 @@ import javax.validation.Valid;
 public class PasswordController {
     private final ResetTokenService resetTokenService;
     private final PasswordEncoder encoder;
-    private final PasswordService passwordService;
+    private final PasswordServiceImpl passwordService;
     private final UserService userService;
 
     @GetMapping("/password")
